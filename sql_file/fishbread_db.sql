b8d8488f795f14d8472bce196cc4d04777d12548
Jamily <ajin20823@gmail.com>
2025-09-03 18:24:30 +0900

    database 2일차 과제 제출

---
+++ sql_file/fishbread_db.sql
@@ -0,0 +1,55 @@
+-- drop user 'admin_user'@'localhost'; 
+# admin_user 생성
+CREATE USER 'admin_user'@'localhost' IDENTIFIED BY 'qwer1234';
+# 모든권한 부여
+GRANT ALL PRIVILEGES ON *.* TO 'admin_user'@'localhost';
+# 해당유저의 부여된 권한 확인
+SHOW GRANTS FOR 'admin_user'@'localhost'; 
+
+# 스키마 생성
+CREATE DATABASE fishbread_db;
+
+USE fishbread_db;
+
+CREATE TABLE users (
+    user_id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    age INT NOT NULL,
+    email VARCHAR(100) UNIQUE,
+    is_business BOOLEAN DEFAULT FALSE
+);
+
+select * from users;
+
+# 주문정보 저장
+CREATE TABLE orders (
+    order_id INT AUTO_INCREMENT PRIMARY KEY,  -- 알아서 값증가
+    user_id INT,
+    order_date DATE,
+    amount DECIMAL(10, 2),
+    FOREIGN KEY (user_id) REFERENCES users(user_id)
+);
+
+CREATE TABLE inventory (
+    item_id INT AUTO_INCREMENT PRIMARY KEY,
+    item_name VARCHAR(255) NOT NULL,
+    quantity INT NOT NULL
+);
+
+CREATE TABLE sales (
+    sale_id INT AUTO_INCREMENT PRIMARY KEY,
+    order_id INT,
+    item_id INT,
+    quantity_sold INT NOT NULL,
+    FOREIGN KEY (order_id) REFERENCES orders(order_id),
+    FOREIGN KEY (item_id) REFERENCES inventory(item_id)
+);
+
+CREATE TABLE daily_sales (
+    date DATE PRIMARY KEY,
+    total_sales DECIMAL(10, 2) NOT NULL
+);
+
+SELECT table_schema, table_name
+FRoM INFORMATION_SCHEMA.TABLES
+WHERE TABLE_SCHEMA = 'fishbread_db';
